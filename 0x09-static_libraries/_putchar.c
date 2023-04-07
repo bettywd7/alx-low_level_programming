16bdaf4ddd38c0931741cae696a89029165ffa90
bettywd7 <bettywonde@gmail.com>
2023-04-07 04:33:42 -0700

    updated

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -4,13 +4,13 @@
 #include <ctype.h>
 #include <stdio.h>
 /**
-*_putchar - write the character c to stdout
-*@c: Print character
-*Return: on success 1.
-*on error, -1 is returned.
-*/
+ * _putchar - write the character c to stdout
+ * @c: Print character
+ * Return: on success 1.
+ * on error, -1 is returned.
+ */
 
 int _putchar(char c)
 {
-return putchar(c);
+		return putchar(c);
 }
